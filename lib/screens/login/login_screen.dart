2b61188e572e14a485c293f3036b698c8a963539
Herick Portugues <herick.portugues@gmail.com>
2020-05-21 16:09:28 -0300

    Finalizando a validação

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/login_bloc.dart';
 import 'package:gerente_loja/screens/login/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -7,6 +8,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _loginBloc = LoginBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,22 +32,32 @@ class _LoginScreenState extends State<LoginScreen> {
                   InputField(
                     icon: Icons.person_outline,
                     hint: "Usuário",
-                    obscure: false
+                    obscure: false,
+                    stream: _loginBloc.outEmail,
+                    onChanged: _loginBloc.changeEmail,
                   ),
                   InputField(
                     icon: Icons.lock_outline,
                     hint: "Senha",
                     obscure: true,
+                    stream: _loginBloc.outPassword,
+                    onChanged: _loginBloc.changePassword,
                   ),
                   SizedBox(height: 32),
-                  SizedBox(
-                    height: 50,
-                    child: RaisedButton(
-                      child: Text("Entrar"),
-                      color: Colors.pinkAccent,
-                      textColor: Colors.white,
-                      onPressed: () {},
-                    ),
+                  StreamBuilder<bool>(
+                    stream: _loginBloc.outSubmitValid,
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        height: 50,
+                        child: RaisedButton(
+                          child: Text("Entrar"),
+                          color: Colors.pinkAccent,
+                          textColor: Colors.white,
+                          onPressed: snapshot.hasData ? () {} : null,
+                          disabledColor: Colors.pinkAccent.withAlpha(140),
+                        ),
+                      );
+                    }
                   )
                 ],
               ),
--- lib/screens/login/widgets/input_field.dart
+++ lib/screens/login/widgets/input_field.dart
@@ -5,30 +5,40 @@ class InputField extends StatelessWidget {
   final IconData icon;
   final String hint;
   final bool obscure;
+  final Stream<String> stream;
+  final Function(String) onChanged;
 
-  InputField({@required this.icon, @required this.hint, @required this.obscure});
+  InputField({@required this.icon, @required this.hint, 
+    @required this.obscure, @required this.stream, @required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 15),
-      child: TextFormField(
-        decoration: InputDecoration(
-          icon: Icon(icon, color: Colors.white),
-          hintText: hint,
-          hintStyle: TextStyle(color: Colors.white),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.pinkAccent)
-          ),
-          contentPadding: EdgeInsets.only(
-            left: 5,
-            right: 15,
-            bottom: 15,
-            top: 15
-          )
-        ),
-        style: TextStyle(color: Colors.white),
-        obscureText: obscure,
+      child: StreamBuilder<String>(
+        stream: stream,
+        builder: (context, snapshot) {
+          return TextFormField(
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              icon: Icon(icon, color: Colors.white),
+              hintText: hint,
+              hintStyle: TextStyle(color: Colors.white),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.pinkAccent)
+              ),
+              contentPadding: EdgeInsets.only(
+                left: 5,
+                right: 15,
+                bottom: 15,
+                top: 15
+              ),
+              errorText: snapshot.hasError ? snapshot.error : null
+            ),
+            style: TextStyle(color: Colors.white),
+            obscureText: obscure,
+          );
+        }
       ),
     );
   }
