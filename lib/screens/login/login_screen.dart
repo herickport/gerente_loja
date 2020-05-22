963f3d3babd255f993d646533650f167934f714b
Herick Portugues <herick.portugues@gmail.com>
2020-05-22 12:04:51 -0300

    Finalizando o Login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/login_bloc.dart';
+import 'package:gerente_loja/screens/home/home_screen.dart';
 import 'package:gerente_loja/screens/login/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,59 +13,106 @@ class _LoginScreenState extends State<LoginScreen> {
   final _loginBloc = LoginBloc();
 
   @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.signOut();
+    _loginBloc.outState.listen((state) {
+      switch(state) {
+        case LoginState.SUCESS:
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => HomeScreen())
+          );
+          break;
+        case LoginState.FAIL:
+          showDialog(context: context, builder: (context) => AlertDialog(
+            title: Text("Erro"),
+            content: Text("Você não possui os privilégios necessários"),
+          ));
+          break;
+        case LoginState.LOADING:
+        case LoginState.IDLE:
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: StreamBuilder<LoginState>(
+        stream: _loginBloc.outState,
+        initialData: LoginState.LOADING,
+        builder: (context, snapshot) {
+          switch(snapshot.data) {
+            case LoginState.LOADING:
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.pinkAccent)
+                )
+              );
+            case LoginState.FAIL:
+            case LoginState.SUCESS:
+            case LoginState.IDLE:
+              return Stack(
+                alignment: Alignment.center,
                 children: <Widget>[
-                  Icon(
-                    Icons.store,
-                    color: Colors.pinkAccent,
-                    size: 160,
+                  Container(),
+                  SingleChildScrollView(
+                    child: Container(
+                      margin: EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(
+                            Icons.store,
+                            color: Colors.pinkAccent,
+                            size: 160,
+                          ),
+                          InputField(
+                            icon: Icons.person_outline,
+                            hint: "Usuário",
+                            obscure: false,
+                            stream: _loginBloc.outEmail,
+                            onChanged: _loginBloc.changeEmail,
+                          ),
+                          InputField(
+                            icon: Icons.lock_outline,
+                            hint: "Senha",
+                            obscure: true,
+                            stream: _loginBloc.outPassword,
+                            onChanged: _loginBloc.changePassword,
+                          ),
+                          SizedBox(height: 32),
+                          StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              return SizedBox(
+                                height: 50,
+                                child: RaisedButton(
+                                  child: Text("Entrar"),
+                                  color: Colors.pinkAccent,
+                                  textColor: Colors.white,
+                                  onPressed: snapshot.hasData ? _loginBloc.submit : null,
+                                  disabledColor: Colors.pinkAccent.withAlpha(140),
+                                ),
+                              );
+                            }
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  InputField(
-                    icon: Icons.person_outline,
-                    hint: "Usuário",
-                    obscure: false,
-                    stream: _loginBloc.outEmail,
-                    onChanged: _loginBloc.changeEmail,
-                  ),
-                  InputField(
-                    icon: Icons.lock_outline,
-                    hint: "Senha",
-                    obscure: true,
-                    stream: _loginBloc.outPassword,
-                    onChanged: _loginBloc.changePassword,
-                  ),
-                  SizedBox(height: 32),
-                  StreamBuilder<bool>(
-                    stream: _loginBloc.outSubmitValid,
-                    builder: (context, snapshot) {
-                      return SizedBox(
-                        height: 50,
-                        child: RaisedButton(
-                          child: Text("Entrar"),
-                          color: Colors.pinkAccent,
-                          textColor: Colors.white,
-                          onPressed: snapshot.hasData ? _loginBloc.submit : null,
-                          disabledColor: Colors.pinkAccent.withAlpha(140),
-                        ),
-                      );
-                    }
-                  )
                 ],
-              ),
-            ),
-          ),
-        ],
+              );
+          }
+        }
       ),
     );
   }
--- lib/validators/login_validators.dart
+++ lib/validators/login_validators.dart
@@ -13,10 +13,10 @@ class LoginValidators {
 
   final validatePassword = StreamTransformer<String, String>.fromHandlers(
     handleData: (password, sink) {
-      if(password.length >= 4) {
+      if(password.length >= 6) {
         sink.add(password);
       } else {
-        sink.addError("Senha inválida, deve conter pelo menos 4 caracteres");
+        sink.addError("A senha deve conter pelo menos 6 caracteres");
       }
     }
   );
