cc652b51b23cf1283e60b5139e034c38067b511a
Herick Portugues <herick.portugues@gmail.com>
2020-05-23 13:35:25 -0300

    Obtendo gasto total

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 
 class UserBloc extends BlocBase {
-  final _userController = BehaviorSubject();
+  final _usersController = BehaviorSubject();
 
   Map<String, Map<String, dynamic>> _users = {};
 
@@ -21,21 +21,52 @@ class UserBloc extends BlocBase {
         switch(change.type) {
           case DocumentChangeType.added:
             _users[uid] = change.document.data;
+            _subscribeToOrders(uid);
             break;
           case DocumentChangeType.modified:
             _users[uid].addAll(change.document.data);
+            _usersController.add(_users.values.toList());
             break;
           case DocumentChangeType.removed:
             _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _usersController.add(_users.values.toList());
             break;
         }
       });
     });
   }
 
+  void _subscribeToOrders(String uid) {
+    _users[uid]["subscription"] =  _firestore.collection("users").document(uid)
+      .collection("orders").snapshots().listen((orders) async {
+        int numOrders = orders.documents.length;
+        double money = 0.0;
+
+        for(DocumentSnapshot d in orders.documents) {
+          DocumentSnapshot order = await _firestore
+            .collection("orders").document(d.documentID).get();
+
+          if(order.data == null) continue;
+
+          money += order.data["totalPrice"];
+        }
+
+        _users[uid].addAll(
+          {"money": money, "orders": numOrders}
+        );
+
+        _usersController.add(_users.values.toList());
+      });
+  }
+
+  void _unsubscribeToOrders(String uid) {
+    _users[uid]["subscription"].cancel();
+  }
+
   @override
   void dispose() {
-    _userController.close();
+    _usersController.close();
   }
 
 }
