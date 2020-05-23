fc17d609f3e99d757bc3d76da48b9158a73879dd
Herick Portugues <herick.portugues@gmail.com>
2020-05-23 15:46:44 -0300

    Exibindo a lista de clientes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/user_bloc.dart';
 import 'package:gerente_loja/tabs/users_tab.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,10 +12,13 @@ class _HomeScreenState extends State<HomeScreen> {
   PageController _pageController;
   int _page = 0;
 
+  UserBloc _userBloc;
+
   @override
   void initState() {
     super.initState();
     _pageController = PageController();
+    _userBloc = UserBloc();
   }
 
   @override
@@ -25,6 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(
           canvasColor: Colors.pinkAccent,
@@ -59,18 +65,21 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (page) {
-            setState(() {
-              _page = page;
-            });
-          },
-          children: <Widget>[
-            UsersTab(),
-            Container(color: Colors.yellow,),
-            Container(color: Colors.green,),
-          ],
+        child: BlocProvider<UserBloc>(
+          bloc: _userBloc,
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (page) {
+              setState(() {
+                _page = page;
+              });
+            },
+            children: <Widget>[
+              UsersTab(),
+              Container(color: Colors.yellow,),
+              Container(color: Colors.green,),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/user_tile.dart
+++ lib/widgets/user_tile.dart
@@ -1,32 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class UserTile extends StatelessWidget {
-  final _textStyle = TextStyle(color: Colors.white);
+  final Map<String, dynamic> user;
+
+  UserTile({@required this.user});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        "title",
-        style: _textStyle,
-      ),
-      subtitle: Text(
-        "subtitle",
-        style: _textStyle,
-      ),
-      trailing: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Text(
-            "Pedidos: 0",
-            style: _textStyle,
-          ),
-          Text(
-            "Gasto: 0",
-            style: _textStyle,
-          )
-        ],
-      ),
-    );
+    final _textStyle = TextStyle(color: Colors.white);
+
+    if(user.containsKey("money"))
+      return ListTile(
+        title: Text(
+          user["name"],
+          style: _textStyle,
+        ),
+        subtitle: Text(
+          user["email"],
+          style: _textStyle,
+        ),
+        trailing: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              "Pedidos: ${user["orders"]}",
+              style: _textStyle,
+            ),
+            Text(
+              "Gasto: R\$${user["money"].toStringAsFixed(2)}",
+              style: _textStyle,
+            )
+          ],
+        ),
+      );
+    else
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              width: 100,
+              height: 20,
+              child: Shimmer.fromColors(
+                child: Container(
+                  color: Colors.white.withAlpha(50),
+                  margin: EdgeInsets.symmetric(vertical: 4),
+                ),
+                baseColor: Colors.white,
+                highlightColor: Colors.grey
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 20,
+              child: Shimmer.fromColors(
+                child: Container(
+                  color: Colors.white.withAlpha(50),
+                  margin: EdgeInsets.symmetric(vertical: 4),
+                ),
+                baseColor: Colors.white,
+                highlightColor: Colors.grey
+              ),
+            )
+          ],
+        ),
+      );
   }
 }
