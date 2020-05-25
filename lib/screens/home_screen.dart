aab6315ea0a1be541427c60e947a73187a13ce75
Herick Portugues <herick.portugues@gmail.com>
2020-05-25 11:39:43 -0300

    Tab de Pedidos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/user_bloc.dart';
+import 'package:gerente_loja/tabs/orders_tab.dart';
 import 'package:gerente_loja/tabs/users_tab.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -76,7 +77,7 @@ class _HomeScreenState extends State<HomeScreen> {
             },
             children: <Widget>[
               UsersTab(),
-              Container(color: Colors.yellow,),
+              OrdersTab(),
               Container(color: Colors.green,),
             ],
           ),
