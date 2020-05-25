3a1532cefe721c6a970ea6c204eaffe4981bddb0
Herick Portugues <herick.portugues@gmail.com>
2020-05-25 12:57:26 -0300

    Mudando o status do pedido

--- lib/widgets/order_tile.dart
+++ lib/widgets/order_tile.dart
@@ -17,6 +17,8 @@ class OrderTile extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
       child: Card(
         child: ExpansionTile(
+          key: Key(order.documentID),
+          initiallyExpanded: order.data["status"] != 4,
           title: Text(
             "${order.documentID.substring(order.documentID.length - 7, order.documentID.length)} - "
               "${states[order.data["status"]]}",
@@ -28,7 +30,7 @@ class OrderTile extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  OrderHeader(),
+                  OrderHeader(order),
                   Column(
                     mainAxisSize: MainAxisSize.min,
                     children: order.data["products"].map<Widget>((p) {
@@ -47,17 +49,25 @@ class OrderTile extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Firestore.instance.collection("users").document(order["clientId"])
+                            .collection("orders").document(order.documentID).delete();
+                          order.reference.delete();
+                        },
                         textColor: Colors.red,
                         child: Text("Excluir")
                       ),
                       FlatButton(
-                        onPressed: () {},
+                        onPressed: order.data["status"] > 1 ? () {
+                          order.reference.updateData({"status": order.data["status"] - 1});
+                        } : null,
                         textColor: Colors.grey[850],
                         child: Text("Regredir")
                       ),
                       FlatButton(
-                        onPressed: () {},
+                        onPressed: order.data["status"] < 4 ? () {
+                          order.reference.updateData({"status": order.data["status"] + 1});
+                        } : null,
                         textColor: Colors.green,
                         child: Text("Avançar")
                       )
