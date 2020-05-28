fc8e8f2749db6bd304eaa4a674457fa95d727206
Herick Portugues <herick.portugues@gmail.com>
2020-05-28 20:05:38 -0300

    Validando os dados da categoria

--- lib/widgets/edit_category_dialog.dart
+++ lib/widgets/edit_category_dialog.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gerente_loja/blocs/category_bloc.dart';
+import 'package:gerente_loja/widgets/image_source_sheet.dart';
 
 class EditCategoryDialog extends StatefulWidget {
   final DocumentSnapshot category;
@@ -35,6 +36,17 @@ class _EditCategoryDialogState extends State<EditCategoryDialog> {
           children: <Widget>[
             ListTile(
               leading: GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => ImageSourceSheet(
+                      onImageSelected: (image) {
+                        Navigator.of(context).pop();
+                        _categoryBloc.setImage(image);
+                      },
+                    )
+                  );
+                },
                 child: StreamBuilder(
                   stream: _categoryBloc.outImage,
                   builder: (context, snapshot) {
@@ -49,8 +61,17 @@ class _EditCategoryDialogState extends State<EditCategoryDialog> {
                   }
                 ),
               ),
-              title: TextField(
-                controller: _controller,
+              title: StreamBuilder<String>(
+                stream: _categoryBloc.outTitle,
+                builder: (context, snapshot) {
+                  return TextField(
+                    controller: _controller,
+                    onChanged: _categoryBloc.setTitle,
+                    decoration: InputDecoration(
+                      errorText: snapshot.hasError ? snapshot.error : null
+                    ),
+                  );
+                }
               ),
             ),
             Row(
@@ -69,11 +90,16 @@ class _EditCategoryDialogState extends State<EditCategoryDialog> {
                     );
                   }
                 ),
-                FlatButton(
-                  onPressed: () {
+                StreamBuilder<bool>(
+                  stream: _categoryBloc.submitValid,
+                  builder: (context, snapshot) {
+                    return FlatButton(
+                      onPressed: snapshot.hasData ? () {
 
-                  },
-                  child: Text("Salvar"),
+                      } : null,
+                      child: Text("Salvar"),
+                    );
+                  }
                 )
               ],
             )
